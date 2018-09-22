4c1aa1e693536de58d78842eadf1c0e395c9cd99
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-09-22 16:05:54 +0200

    add lyrics file

---
+++ app.py
@@ -0,0 +1,15 @@
+from util import *
+
+#text_input = 'Try to chase me\nSo call me, maybe\nHey I just met you\n'
+#sentence_to_music(text_input)
+    
+
+
+
+text_verse = 'We were the kings and queens of promise We were the victims of ourselves'
+generative_text = 'We Have only broken our dreams'
+generative_other = 'bad and this is crazy but you look right'
+generative_rap = 'You want it for a long time'
+text = generative_other
+  
+words_to_music(text)
